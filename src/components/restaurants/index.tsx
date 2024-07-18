3109185b4c93650845237261b048d7bda906b560
fabiodrneles <fabiodrneles@gmail.com>
2024-07-18 16:33:04 -0300

    corrigindo ip para retorno local da fake api no horizontal list

--- src/components/restaurants/index.tsx
+++ src/components/restaurants/index.tsx
@@ -13,7 +13,7 @@ export function Restaurants() {
 
   useEffect(() => {
     async function getFoods(){
-      const response = await fetch("http://192.168.0.12:3000/restaurants")
+      const response = await fetch("http://192.168.100.17:3000/restaurants")
       const data = await response.json()
       setRestaurants(data);
     }
